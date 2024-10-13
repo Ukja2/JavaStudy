86a501778a9a2adae667932a90318aecd73d1311
qwe02 <qwe02@DESKTOP-BAAN5R9>
2024-10-13 12:02:13 +0900

    Final

--- day1/src/Final.java
+++ day1/src/Final.java
@@ -1,9 +1,8 @@
-
 public class Final {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		final int hour = 15;//"final"은 상수로 그 값을 선언했으면 이후에 값을 수정할 수 없음 
+		hour = 20; // 위에서 상수로 지정된 자료형 변수명 int에 15를 저장했기 때문에 20으로 다시 값을 저장할 수 없음 
 	}
 
 }
