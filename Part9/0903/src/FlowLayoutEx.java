3086526900985532b9393a6edd7813627903b4c7
JaeUkLee <157056310+Ukja2@users.noreply.github.com>
2024-12-04 00:25:39 +0900

    FlowLayout 배치관리자 활용

---
+++ Part9/0903/src/FlowLayoutEx.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayoutEx extends JFrame{
+	public FlowLayoutEx() {
+		setTitle("FlowLayout Sample");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		
+		
+		/*c.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));
+		c.add(new JButton("add"));
+		c.add(new JButton("sub"));
+		c.add(new JButton("mul"));
+		c.add(new JButton("div"));
+		c.add(new JButton("Calculate"));
+		*/
+		
+		
+		c.setLayout(new BorderLayout(30, 20));
+		c.add(new JButton("Calculate"), BorderLayout.CENTER);
+		c.add(new JButton("add"), BorderLayout.NORTH);
+		c.add(new JButton("sub"), BorderLayout.SOUTH);
+		c.add(new JButton("mul"), BorderLayout.EAST);
+		c.add(new JButton("div"), BorderLayout.WEST);
+		
+		setSize(300, 200);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new FlowLayoutEx();
+	}
+}
